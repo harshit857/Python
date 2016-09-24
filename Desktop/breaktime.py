ddda98baa967f64022f1b2bc6f366b4470e3ccee
Harshit Goyal <harshit857@users.noreply.github.com>
2016-09-24 23:38:38 +0530

    Update breaktime.py

--- Desktop/breaktime.py
+++ Desktop/breaktime.py
@@ -1,9 +1,10 @@
+# Program to play Youtube video (Bulleya song) after every 200 seconds i.e.(3min 26sec )
 import webbrowser
 import time
 
 count = 0
 print("This program started on "+time.ctime())
 while count<3:
-    time.sleep(5)
+    time.sleep(200)
     webbrowser.open("https://www.youtube.com/watch?v=hXh35CtnSyU")
     count += 1
